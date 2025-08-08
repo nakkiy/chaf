fad8508de11530bf83d3ca3d234ffc0049aefb2b
nakkiy <48021938+nakkiy@users.noreply.github.com>
2025-08-08 20:32:08 +0900

    release 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 // Expose engine and core modules for external tests
+#![doc = include_str!("../README.md")]
+#[doc(hidden)]
 pub mod cli;
+#[doc(hidden)]
 pub mod core;
+#[doc(hidden)]
 pub mod engine;
